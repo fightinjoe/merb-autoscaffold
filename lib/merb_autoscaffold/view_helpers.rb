9eaabd7a1f9037e30d3ec351f283f7b17a844b5d
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-04-26 12:49:39 +0900

    all table-related methods namespaced and moved to the helpers

--- lib/merb_autoscaffold/view_helpers.rb
+++ lib/merb_autoscaffold/view_helpers.rb
@@ -3,5 +3,37 @@ module Merb
     def scaf_title( obj )
       obj.try(:title) || obj.try(:name) || "#{ obj.class.to_s } ##{ obj.id }"
     end
+
+    def scaf_link( obj )
+      <<-EOS
+      <a href="#{ url( obj.class.singular_name, obj ) }">
+        #{ scaf_title( obj ) }
+      </a>
+      EOS
+    end
+
+    def scaf_table()   database.schema[ self.class.Model ]; end
+    def scaf_columns() scaf_table.columns;                       end
+    def scaf_assocs()  scaf_table.associations;                  end
+    def scaf_has_manys
+      scaf_assocs.select { |a|
+        a.is_a?(DataMapper::Associations::HasManyAssociation)
+      }
+    end
+
+    def scaf_belongs_tos
+      scaf_assocs.select { |a|
+        a.is_a?(DataMapper::Associations::BelongsToAssociation)
+      }
+    end
+
+    def scaf_assoc_hash
+      Hash[*scaf_belongs_tos.collect {|a| [a.foreign_key_name, a]}.flatten]
+    end
+
+    def scaf_foreign_keys
+      scaf_belongs_tos.collect(&:foreign_key_name)
+    end
+
   end
 end
