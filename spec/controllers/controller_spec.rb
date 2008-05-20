0d1cc8cca98cd25162181736fe10d387210218c5
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-05-20 21:27:03 +0900

    Added full controller tests

--- spec/controllers/controller_spec.rb
+++ spec/controllers/controller_spec.rb
@@ -10,31 +10,73 @@ end
 describe "controller CRUD actions" do
   before(:each) do
     Blog.delete_all
+    @b1 = Blog.create({})
+    @b2 = Blog.create({})
   end
 
   it "should respond to index" do
-    b1 = Blog.create({})
-    b2 = Blog.create({})
-
-    controller = dispatch_to( Scaffold::Blogs, :index ) { |r| stub_request_for(r, :render) }
+    controller = blog_request( :index )
 
     controller.status.should == 200
 
     models = controller.assigns(:models).page(nil)
-    models.collect(&:id).should == [b1, b2].collect(&:id)
+    models.collect(&:id).should == [@b1, @b2].collect(&:id)
   end
 
-  it "should respond to show"
+  it "should respond to show" do
+    controller = blog_request( :show, :id => @b1.id )
 
-  it "should respond to new"
+    controller.status.should == 200
+    controller.assigns(:model).should == @b1
+  end
 
-  it "should respond to create"
+  it "should respond to new" do
+    controller = blog_request( :new )
 
-  it "should respond to edit"
+    controller.status.should == 200
+    controller.assigns(:model).should == Blog.new
+  end
 
-  it "should respond to update"
+  it "should create a new blog" do
+    count = Blog.count
+    controller = blog_request( :create, :model => blog_options )
 
-  it "should respond to delete"
+    controller.status.should == 302
+    Blog.count.should == count + 1
+  end
+
+  it "should respond to edit" do
+    controller = blog_request( :edit, :id => @b1.id )
+
+    controller.status.should == 200
+    controller.assigns(:model).should == @b1
+  end
+
+  it "should update the blog" do
+    title  = 'new title'
+    params = {:id => @b1.id, :model => blog_options.merge( :title => title )}
+    controller = blog_request( :update, params )
+
+    controller.status.should          == 302
+    controller.assigns(:model).should == @b1
+    @b1.reload.title.should           == title
+  end
+
+  it "should delete the blog" do
+    count = Blog.count
+    controller = blog_request( :destroy, :id => @b1.id )
+
+    controller.status.should == 302
+    Blog.count.should == count - 1
+  end
+end
+
+def blog_request( action, params = {})
+  dispatch_to( Scaffold::Blogs, action, params ) { |r| stub_request_for(r, :render) }
+end
+
+def blog_options
+  { :title => 'title' }
 end
 
 def stub_request_for( req, *opts )
